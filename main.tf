7e00f1ac8b8a705d4502a5f5a05cbb922d9771b1
DouglasHFonseca <68574857+DouglasHFonseca@users.noreply.github.com>
2023-07-24 15:14:28 -0300

    try again

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = false
@@ -36,6 +35,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -48,7 +48,7 @@ module "blog_sg" {
   version = "5.1.0"
   
   name   ="blog"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
